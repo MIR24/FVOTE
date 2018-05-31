8bca7b90c2111f4a1724904540a06b050abd9445
volcanolog <volcan0@mail.ru>
2018-05-31 11:34:45 +0300

    https://mir24tv.atlassian.net/browse/VOTE-36

--- app/Forms/WorkForm.php
+++ app/Forms/WorkForm.php
@@ -10,9 +10,9 @@ class WorkForm extends Form
     public function buildForm()
     {
         $this
-            ->add('name', 'textarea', ['label' => 'Имя', 'rules' => 'required|string|max:255'])
+            ->add('name', 'text', ['label' => 'Имя', 'rules' => 'required|string|max:255'])
             ->add('url', 'text', ['label' => 'Ссылка на сюжет', 'rules' => 'required|string|max:255'])
-            ->add('filial', 'textarea', ['label' => 'Филиал', 'rules' => 'required|string|max:255'])
+            ->add('filial', 'text', ['label' => 'Филиал', 'rules' => 'required|string|max:255'])
             ->add('correspondent', 'text', ['label' => 'Корреспондент', 'rules' => 'required|string|max:255'])
             ->add('operator', 'text', ['label' => 'Оператор', 'rules' => 'required|string|max:255'])
             ->add('nomimation', 'hidden', ['value' => $this->getData('nomimation')])

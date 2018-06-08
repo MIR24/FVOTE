b22c92a725d3c858e9aaf4d1aa6eea5b3830e7d9
Pavel Belyaev <volcan0@mail.ru>
2018-06-08 20:42:50 +0300

    https://mir24tv.atlassian.net/browse/VOTE-47

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,45 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-/*Route::get('/', function () {
-    return view('home'); // welcome
-});*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
+/* Route::get('/', function () {
+  return view('home'); // welcome
+  }); */
 Route::redirect('/', '/nominations', 301);
 
 Route::middleware(['auth'])->group(function () {
+    Route::get('/nominations/{id}/result', 'WorkController@printResult')
+            ->where('id', '[0-9]+')
+            ->name('works.printResult');
+
     Route::get('/nominations/{id}/works', 'WorkController@indexByNomination')
-        ->where('id', '[0-9]+')
-        ->name('works.indexByNomination');
+            ->where('id', '[0-9]+')
+            ->name('works.indexByNomination');
 
     Route::get('/nominations/{nId}/works/{wId}/thumbsup', 'WorkController@thumbsUp')
-        ->where(['nid' => '[0-9]+', 'wId' => '[0-9]+'])
-        ->name('works.thumbsUp');
+            ->where(['nid' => '[0-9]+', 'wId' => '[0-9]+'])
+            ->name('works.thumbsUp');
 
     Route::get('/api/users', 'UserController@indexDT')
-        ->name('api.users.index');
+            ->name('api.users.index');
 
     Route::get('/api/nominations', 'NominationController@indexDT')
-        ->name('api.nominations.index');
+            ->name('api.nominations.index');
 
     Route::get('/api/works', 'WorkController@indexDT')
-        ->name('api.works.index');
+            ->name('api.works.index');
 
     Route::get('/api/nominations/{id}/works', 'WorkController@indexByNominationDT')
-        ->where('id', '[0-9]+')
-        ->name('api.works.indexByNomination');
+            ->where('id', '[0-9]+')
+            ->name('api.works.indexByNomination');
 
     Route::get('/works/create/{Id}', 'WorkController@create')->where('id', '[0-9]+');
 

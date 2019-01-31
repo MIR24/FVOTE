891b0af85968e03ff80c1409a013e492f506c77c
Konshin Alexander <kon2work@gmail.com>
2019-01-31 17:30:27 +0300

    Add Producer datatables views and form.

---
+++ app/Forms/WorkFormProducer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Forms;
+
+use Kris\LaravelFormBuilder\Form;
+use App\Nomination;
+
+class WorkFormProducer extends Form
+{
+    public function buildForm()
+    {
+        $this
+            ->add('filial', 'text', ['label' => 'Филиал', 'rules' => 'required|string|max:255'])
+            ->add('correspondent', 'text', ['label' => 'Номинант', 'rules' => 'required|string|max:255'])
+            ->add('name', 'text', ['label' => 'Обоснование', 'rules' => 'required|string|max:255'])
+            ->add('nomination', 'hidden', ['value' => $this->getData('nomination')])
+            ->add('submit', 'submit', ['label' => 'Принять'])
+            ->add('reset', 'reset', ['label' => 'Очистить']);
+    }
+}
